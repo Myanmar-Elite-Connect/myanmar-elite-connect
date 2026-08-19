7e58a50975def7733433163fe9123d3b8112b4f6
Myanmar-Elite-Connect <thitlinn680@gmail.com>
2026-08-19 19:25:17 +0630

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,30 +1,11 @@
-/* =====================================================
-   MYANMAR ELITE CONNECT
-   SUPABASE CONNECTION
-===================================================== */
-
 const SUPABASE_URL =
   "https://ikjwisfsdcupibgjiuvp.supabase.co";
 
 const SUPABASE_KEY =
   "sb_publishable_xoh1rjwBlJhR9nB3kQD-KA_QcVY6w2s";
 
-
-/*
- * Create Supabase client
- */
-
-const supabaseClient =
+window.supabaseClient =
   window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_KEY
   );
-
-
-/*
- * Make client available to
- * every page script.
- */
-
-window.supabaseClient =
-  supabaseClient;
