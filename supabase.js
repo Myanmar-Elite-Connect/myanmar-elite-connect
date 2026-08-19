dabd1cc67847bc6b982a32308d247a1551ddc504
Myanmar-Elite-Connect <thitlinn680@gmail.com>
2026-08-19 12:01:53 +0630

    Create supabase.js

---
+++ supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://ikjwisfsdcupibgjiuvp.supabase.co";
+const SUPABASE_KEY = "မင်းရဲ့ Publishable Key ကို ဒီနေရာမှာ paste လုပ်ပါ";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
