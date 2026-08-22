1994c0cec53aaa18a3ecc0802e5f909f56c5c660
Myanmar-Elite-Connect <thitlinn680@gmail.com>
2026-08-22 23:45:19 +0630

    Remove conflicting add-listing navigation injection

--- mec-theme.js
+++ mec-theme.js
@@ -6,14 +6,3 @@
   s.dataset.mecThemesLoader='1';
   (document.head||document.documentElement).appendChild(s);
 })();
-
-/* Add-listing uses the four-item mobile navigation: Home / Add Listing / Chat / Profile. */
-(function(){
-  const page=(location.pathname.split('/').pop()||'').toLowerCase();
-  if(page!=='add-listing.html') return;
-  if(document.querySelector('script[data-add-listing-nav]')) return;
-  const s=document.createElement('script');
-  s.src='./add-listing-nav.js';
-  s.dataset.addListingNav='1';
-  (document.head||document.documentElement).appendChild(s);
-})();
